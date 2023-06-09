2ee970075887b760a18e529baac06191c071d691
nguyenductri1108 <73732855+nguyenductri1108@users.noreply.github.com>
2023-06-09 18:44:55 +0700

    set up

--- src/index.js
+++ src/index.js
@@ -1,28 +1,31 @@
-const express = require("express");
-const morgan = require("morgan");
-const handlebars = require("express-handlebars").engine;
-const path = require("path");
+const express = require('express');
+const morgan = require('morgan');
+const handlebars = require('express-handlebars').engine;
+const path = require('path');
 const app = express();
 const port = 3000;
 
+const route = require('./routes');
+
 //HTTP logger
-app.use(morgan("combined"));
+app.use(morgan('combined'));
 
 //template engine
 app.engine(
-    "hbs",
-    handlebars({
-        extname: ".hbs",
-    })
+  'hbs',
+  handlebars({
+    extname: '.hbs',
+  }),
 );
-app.set("view engine", "hbs");
-app.use(express.static(path.join(__dirname, "public")));
-app.set("views", path.join(__dirname, "resources/views"));
+app.set('view engine', 'hbs');
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'resources/views'));
 
-app.get("/", (req, res) => {
-    return res.render("home");
+route(app);
+app.get('/', (req, res) => {
+  return res.render('home');
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+  console.log(`Example app listening on port ${port}`);
 });
